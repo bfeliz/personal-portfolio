5304fde021ea7ec524918695e814c464d7233ec9
Brittany Fortner <brittany.e.fortner@gmail.com>
2020-02-27 13:16:52 -0700

    Add sub-row responsive functions

--- js/script.js
+++ js/script.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
+    // keep dropdown from covering menu button
     $(".dropdown-trigger").dropdown({ coverTrigger: false });
+
+    // keep bottom columns same height
+    function sameHeights(selector) {
+        var selector = selector || '[data-key="sameHeights"]',
+            query = document.querySelectorAll(selector),
+            elements = query.length,
+            max = 0;
+        if (elements) {
+            while (elements--) {
+                var element = query[elements];
+                if (element.clientHeight > max) {
+                    max = element.clientHeight;
+                }
+            }
+            elements = query.length;
+            while (elements--) {
+                var element = query[elements];
+                element.style.height = max + "px";
+            }
+        }
+    }
+    // event listener for bottom columns
+    if ("addEventListener" in window) {
+        window.addEventListener("resize", function() {
+            sameHeights();
+        });
+        window.addEventListener("load", function() {
+            sameHeights();
+        });
+    }
 });
